1bcbde99e063dd50a2a4d4e065011795d9b39e67
pankaj kore <korep95@gmail.com>
2022-02-21 18:15:59 +0530

    [pankaj]created stack of 56->30->70

---
+++ src/main/java/com/LinkedList/MyStack.java
@@ -0,0 +1,36 @@
+package com.LinkedList;
+
+public class MyStack {
+    private final MyLinkedList myLinkedList;
+
+    /**
+     * constructor Created
+     */
+    public MyStack() {
+        this.myLinkedList=new MyLinkedList();
+    }
+
+    /**
+     * Adding node in stack using LinkedList
+     * @param myNode
+     */
+    public void push(INode myNode) {
+        myLinkedList.add(myNode);
+
+    }
+
+    /**
+     * Displaying Stack using printStack method
+     */
+    public void printStack() {
+        myLinkedList.printMyNode();
+    }
+
+    /**
+     * Returning the First element in stack without deleting.
+     * @return
+     */
+    public INode peak() {
+        return myLinkedList.head;
+    }
+}
